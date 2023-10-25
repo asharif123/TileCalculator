23640600ec38299782871559195fffe0ac5d2c44
Awad Sharif <awadsharif9@gmail.com>
2023-10-24 19:55:45 -0700

    updated variable names to avoid repetition

--- TileCalculator/Program.cs
+++ TileCalculator/Program.cs
@@ -7,30 +7,44 @@
 //Pick ONE ADDITIONAL SHAPE (triangle / circle / etc) and implement the second shape,
 //making the user select wich one they want to calculate
 //onsole.WriteLine("{0} this is an {1} is your name? {2}", aString, panela, large);
-string width;
-string length;
-string costPerUnit;
 
-Console.WriteLine("Enter the width: \n");
-width = Console.ReadLine();
+namespace TileCa1culator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string width;
+            string length;
+            string costPerUnit;
 
-Console.WriteLine("\nEnter the length: \n");
-length = Console.ReadLine();
+            Console.WriteLine("Enter the width: \n");
+            //take user input of width
+            width = Console.ReadLine();
 
-Console.WriteLine("\nEnter the cost per 1 unit of flooring: \n");
-costPerUnit = Console.ReadLine();
+            Console.WriteLine("\nEnter the length: \n");
+            //take user input of length
+            length = Console.ReadLine();
 
-//convert string to float
+            Console.WriteLine("\nEnter the cost per 1 unit of flooring: \n");
+            //take user input of costPerUnit
+            costPerUnit = Console.ReadLine();
 
-float widthVal = float.Parse(width);
-float lengthVal = float.Parse(length);
+            //convert string to float
 
-//convert str to float
-float costPerUnitVal = float.Parse(costPerUnit);
+            float widthVal = float.Parse(width);
+            float lengthVal = float.Parse(length);
+
+            //convert str to float
+            float costPerUnitVal = float.Parse(costPerUnit);
 
 
-float total = widthVal*lengthVal*costPerUnitVal;
+            float total = widthVal * lengthVal * costPerUnitVal;
 
-//comput the total cost, round to 2 digits
-Console.WriteLine("\nThe total cost is ${0}", Math.Round(total,2));
+            //comput the total cost, round to 2 digits
+            Console.WriteLine("\nThe total cost is ${0}", Math.Round(total, 2));
 
+
+        }
+    }
+}
