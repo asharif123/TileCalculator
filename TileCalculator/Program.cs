8054cc82c379854fc21d503ffd2110f7065fecd5
asharif123 <awadsharif9@gmail.com>
2024-10-12 22:06:45 -0700

    updated comments in TileCalculator

--- TileCalculator/Program.cs
+++ TileCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Ask the user to enter in width, length, and the cost per 1 unit of flooring.
-//Have the program calculate how much it would cost to cover the area specified with the flooring.
+//Have the program calculate how much it would cost to cover the area
+//specified with the flooring.
 
 //Added Difficulty: Calculate how much flooring would be needed for non-rectangular rooms.
 //Also figure out how much labor costs would be given that
